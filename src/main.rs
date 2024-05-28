3d786367044682d742bb11c4a440ab4c58f8eff1
Sanger Steel <sangersteel@gmail.com>
2024-05-28 08:46:03 -0400

    tests: Add compatible matrices test

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ impl fmt::Display for Matrix {
 fn main() {
     let a1: Vec<i32> = vec![1,2];
     let a2: Vec<i32> = vec![4,5];
-    let b1: Vec<i32> = vec![7,8];
+    let b1: Vec<i32> = vec![7,8,9];
     let b2: Vec<i32> = vec![10,11];
     let a_mat = Matrix::new(vec![a1, a2]);
     let b_mat = Matrix::new(vec![b1, b2]);
@@ -57,8 +57,18 @@ fn has_uniform_row_length(data: &Vec<Vec<i32>>) -> bool {
     true
 }
 
+fn is_matmul_compatible(a: &Matrix, b: &Matrix) -> bool {
+    let num_cols_a: usize = a.data[0].len();
+    let num_rows_b: usize = b.data.len();
+
+    num_cols_a == num_rows_b
+}
+
 fn matmul(a: &Matrix, b: &Matrix) -> Matrix {
     let mut c_data: Vec<Vec<i32>> = vec![];
+    if !is_matmul_compatible(a, b) {
+        panic!("Matrices are not compatible for multiplication")
+    }
     for i in 0..a.data.len() {
         let mut c_row: Vec<i32> = vec![];
         for j in 0..a.data[i].len() {
@@ -80,3 +90,19 @@ fn inner_product(a: &Vec<i32>, b: &Vec<i32>) -> i32 {
     c
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_matmul_compatible() {
+        let compat_a = Matrix { data:  vec![vec![1,2],vec![3,4]] };
+        let compat_b = Matrix  { data:  vec![vec![1,2,3,4],vec![3,4,5,7]] };    
+        assert_eq!(true, is_matmul_compatible(&compat_a, &compat_b));
+
+        let incompat_a = Matrix { data: vec![vec![1,2],vec![3,4]] };
+        let incompat_b = Matrix { data: vec![vec![1,2],vec![3,4], vec![5,6]] };
+        assert_ne!(true, is_matmul_compatible(&incompat_a, &incompat_b));
+
+    }
+} 
