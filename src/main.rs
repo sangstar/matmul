6a26fb6b923855a5c271c6d7e0f211fe813ac7ac
Sanger Steel <sangersteel@gmail.com>
2024-05-27 20:20:32 -0400

    fix: Rectify incorrect element indexing

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,24 @@ struct Matrix {
 impl Matrix {
     fn new(data: Vec<Vec<i32>>) -> Matrix {
         if !has_uniform_row_length(&data) {
-            panic!("vectors must have same length");
+            panic!("Vectors must have same length");
         }
         let matrix = Matrix { data };
         matrix
+    }
+
+    fn get_column_vector(&self, col_index: i32) -> Vec<i32> {
+        let mut col: Vec<i32> = vec![];
+        for i in 0..self.data.len() {
+            col.push(self.data[i][col_index as usize]);
+        }
+        col
     }
 }
 
 impl fmt::Display for Matrix {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        println!("\n");
         for row in &self.data {
             for elem in row {
                 println!("{}", elem);
@@ -27,10 +36,10 @@ impl fmt::Display for Matrix {
 }
 
 fn main() {
-    let a1: Vec<i32> = vec![1,2,3];
-    let a2: Vec<i32> = vec![4,5,6];
-    let b1: Vec<i32> = vec![7,8,9];
-    let b2: Vec<i32> = vec![10,11,12];
+    let a1: Vec<i32> = vec![1,2];
+    let a2: Vec<i32> = vec![4,5];
+    let b1: Vec<i32> = vec![7,8];
+    let b2: Vec<i32> = vec![10,11];
     let a_mat = Matrix::new(vec![a1, a2]);
     let b_mat = Matrix::new(vec![b1, b2]);
     println!("Matmul is {}", matmul(&a_mat, &b_mat));
@@ -50,10 +59,11 @@ fn has_uniform_row_length(data: &Vec<Vec<i32>>) -> bool {
 
 fn matmul(a: &Matrix, b: &Matrix) -> Matrix {
     let mut c_data: Vec<Vec<i32>> = vec![];
-    for i in 0..a.data.len()-1 {
+    for i in 0..a.data.len() {
         let mut c_row: Vec<i32> = vec![];
-        for j in 0..a.data[i].len()-1 {
-            c_row.push(a.data[i][j] * b.data[j][i]);
+        for j in 0..a.data[i].len() {
+            let b_col: Vec<i32> = b.get_column_vector(j as i32);
+            c_row.push(inner_product(&a.data[i], &b_col));
         }
         c_data.push(c_row);
     }
@@ -62,7 +72,6 @@ fn matmul(a: &Matrix, b: &Matrix) -> Matrix {
 }
 
 
-// Possibly not needed
 fn inner_product(a: &Vec<i32>, b: &Vec<i32>) -> i32 {
     let mut c: i32 = 0;
     for i in 0..a.len() {
