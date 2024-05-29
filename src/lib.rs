23e7e58a16525ac863ba45c38e8c360fee6dcbcd
Sanger Steel <sangersteel@gmail.com>
2024-05-29 00:23:06 -0400

    style: Rename `matmul.rs` to `linalg.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-mod matmul;
-use matmul::Matrix;
+mod linalg;
+use linalg::Matrix;
 
 #[pymodule]
 fn rublas(py: Python, m: &PyModule) -> PyResult<()> {
