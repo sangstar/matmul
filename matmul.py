a26e8b0632021d020315174bf2272250f430bff5
Sanger Steel <sangersteel@gmail.com>
2024-05-28 23:30:41 -0400

    refactor: Change module name

---
+++ matmul.py
@@ -0,0 +1,15 @@
+import rublas
+
+mat_a = [
+    [1,2],
+    [4,5],
+    [1,2],
+]
+
+mat_b = [
+    [7,8,9,46],
+    [10,11,3,12],
+]
+
+if __name__ == "__main__":
+    print(rublas.matmul(mat_a, mat_b))
