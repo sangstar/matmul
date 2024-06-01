6c5dd35be382a5d5e350022e681971f63aaba792
Sanger Steel <sangersteel@gmail.com>
2024-06-01 12:33:10 -0400

    fix: Remove deprecated `matmul`

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,5 @@ use linalg::Matrix;
 #[pymodule]
 fn rublas(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Matrix>()?;
-    m.add_function(wrap_pyfunction!(matmul, m)?)?;
     Ok(())
-}
-
-#[pyfunction]
-fn matmul(a: Vec<Vec<i32>>, b: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mat_a: Matrix = Matrix::new(a);
-    let mat_b: Matrix = Matrix::new(b);
-
-    let c = mat_a * mat_b;
-    c.data
 }
